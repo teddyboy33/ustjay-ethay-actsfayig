8279081ee1eb5d17d3211cde734e3f1bbadd1e9c
Philip Korte <kortephilip@gmail.com>
2020-05-03 14:29:13 -0700

    finished assignment

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+"""
+Tranlates a random fact into pig latin
+Created by Philip Korte
+"""
+
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
 def get_fact():
-
+    """
+    Retrieves a random fact from unkno.com
+    """
     response = requests.get("http://unkno.com")
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -17,12 +24,29 @@ def get_fact():
     return facts[0].getText()
 
 
+def get_location(fact):
+    """
+    Retrieves the location of a piglatinized fact.
+    """
+    url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+    myobj = {'input_text': fact}
+
+    response = requests.post(url, myobj, allow_redirects=False)
+
+    my_location = response.headers['Location']
+    return my_location
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    """
+    Returns a web page with a random fact and a link to it's translation.
+    """
+    fact = get_fact()
+    url = get_location(fact)
+    return render_template('translate.jinja2', fact=fact, url=url)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
